699fd2a9edf8eb6bdba1e40b8c90e800eaaaba25
Erwin <Erwin@erwinrussel.com>
2020-02-25 16:09:15 +0100

    create dedicated css and js folders

--- index.php
+++ index.php
@@ -52,11 +52,10 @@ add_action('wp_enqueue_scripts', 'callback_for_setting_up_scripts');
 function callback_for_setting_up_scripts() {
     wp_enqueue_style( 'slick-css', plugins_url() . '/wordpress-IGTV/slick/slick/slick.css');
     wp_enqueue_style( 'slick-css-theme', plugins_url() . '/wordpress-IGTV/slick/slick/slick-theme.css', 'slick-css', true );
-    wp_enqueue_style( 'IGTVstyle', plugins_url() . '/wordpress-IGTV/IGTVstyle.css', array('slick-css','slick-css-theme'), true );
-    // wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css', '3.4.1', true );
+    wp_enqueue_style( 'IGTVstyle', plugins_url() . '/wordpress-IGTV/css/IGTVstyle.css', array('slick-css','slick-css-theme'), true );
     wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js', array('jquery'), '3.4.1', true );
     wp_enqueue_script( 'slickjs', plugins_url() . '/wordpress-IGTV/slick/slick/slick.js', array ( 'jquery' ), false ,true);
-    wp_enqueue_script('IGTVscript', plugins_url() .'/wordpress-IGTV/IGTVscript.js', array ( 'jquery' ), false ,true);
+    wp_enqueue_script('IGTVscript', plugins_url() .'/wordpress-IGTV/js/IGTVscript.js', array ( 'jquery' ), false ,true);
 }
 
 // IGTV REQUEST

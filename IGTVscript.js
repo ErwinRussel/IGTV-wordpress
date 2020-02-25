e99b0a18cd21cd0c77a6f42c95734c66a63df198
Erwin <Erwin@erwinrussel.com>
2020-02-25 17:16:11 +0100

    add slide amount init

--- IGTVscript.js
+++ IGTVscript.js
@@ -1,7 +1,15 @@
 (function($) {
   $(document).on('ready', function() {
 
-      var slidesToShow = 6;
+      if ($(window).width() > 1200) {
+        slidesToShow = 4;
+      } else if ($(window).width() > 992) {
+        slidesToShow = 4;
+      } else if ($(window).width() > 768) {
+        slidesToShow = 3;
+      } else  {
+        slidesToShow = 1;
+      } 
 
       $(window).resize(function(){
         if ($(window).width() > 1200) {
